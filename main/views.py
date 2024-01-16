3a9370ce147fd28d80d64114eae6bdb5fe14ac46
Eve Braun <eve_braun@mail.ru>
2024-01-16 21:54:00 +0300

    django 16/01

--- main/views.py
+++ main/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
+menu = [{'title': 'Posts', 'url_name': 'main:post_list'}, 
+        # {'title': 'Add posts', 'url_name': 'main:post_add'},
+        # {'title': 'About site', 'url_name': 'main:about'},
+        # {'title': 'Contacts', 'url_name': 'main:contacts'},
+        ]
+
+def index_root(request):
+    return render(request, 'main/index.html')
+
 def index(request):
     return render(request, 'main/index.html')
+
+# ДЗ!!!
+def about_site(request):
+    pass
+
+# отображение списка постов
+def post_list(request):
+    # получаем все объекты таблицы(модели) Post
+    posts = Post.objects.all()
+    # заносим их  вобъкт-контекста для передачи в шаблон
+    context = {'posts': posts, 'menu': menu}
+    return render(request, template_name='main/post_list.html', context=context)
