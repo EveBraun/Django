91e572911c2b3c39cf805e30b2efba604b51562d
Eve Braun <eve_braun@mail.ru>
2024-01-23 21:55:32 +0300

    f4

--- blog/settings.py
+++ blog/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'sorl.thumbnail'
+    'sorl.thumbnail',
+    'bootstrap4',
 ]
 
 # добавили стороннее приложение thumbnail
